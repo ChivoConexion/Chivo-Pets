2b88adf8f9424296e61da0b8a7490f9ebe828e85
ISMAELRIVAS2018 <rivas.ivan2018@gmail.com>
2022-08-07 16:29:56 -0600

    login con estilo terminado

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -10,17 +10,15 @@ const ResetPassword = () => {
                     <span class="r s"></span>
                     <span class="r"></span>
                 </div>
-                <div class="card">
-                    <div class="card-body text-center">
-                        <div class="mb-4">
-                            <i class="feather icon-mail auth-icon"></i>
-                        </div>
-                        <h3 class="mb-4">Reset Password</h3>
+                <div class="card fondo">
+                    <div class="card-body text-center fondo">
+                       
+                        <h4 class="mb-4 color1">Restablecer  contraseña</h4>
                         <div class="input-group mb-3">
                             <input type="email" class="form-control" placeholder="Email" />
                         </div>
-                        <button class="btn btn-primary mb-4 shadow-2">Reset Password</button>
-                        <p class="mb-0 text-muted">Don’t have an account? <a href="auth-signup.html">Signup</a></p>
+                        <button class="btn btn-primary mb-4 shadow-2">Restablecer </button>
+                        <p class="mb-0 text-muted">¿No tienes una cuenta? <a href="auth-signup.html">Inscribirse</a></p>
                     </div>
                 </div>
             </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,49 +1,53 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/img/chivo-pest.png";
 
 const SignUp = () => {
-    return (
-        <div className="auth-wrapper">
-            <div className="auth-content">
-                <div className="auth-bg">
-                    <span className="r"></span>
-                    <span className="r s"></span>
-                    <span className="r s"></span>
-                    <span className="r"></span>
-                </div>
-                <div className="card">
-                    <div className="card-body text-center">
-                        <div className="mb-4">
-                            <i className="feather icon-user-plus auth-icon"></i>
-                        </div>
-                        <h3 className="mb-4">Sign up</h3>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Username" />
-                        </div>
-                        <div className="input-group mb-3">
-                            <input type="email" className="form-control" placeholder="Email" />
-                        </div>
-                        <div className="input-group mb-4">
-                            <input type="password" className="form-control" placeholder="password" />
-                        </div>
-                        <div className="form-group text-left">
-                            <div className="checkbox checkbox-fill d-inline">
-                                <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-1" checked="" />
-                                <label for="checkbox-fill-1" className="cr"> Save Details</label>
-                            </div>
-                        </div>
-                        <div className="form-group text-left">
-                            <div className="checkbox checkbox-fill d-inline">
-                                <input type="checkbox" name="checkbox-fill-2" id="checkbox-fill-2" />
-                                <label for="checkbox-fill-2" className="cr">Send me the <a href="#!"> Newsletter</a> weekly.</label>
-                            </div>
-                        </div>
-                        <button className="btn btn-primary shadow-2 mb-4">Sign up</button>
-                        <p className="mb-0 text-muted">Allready have an account? <a href="auth-signin.html"> Log in</a></p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-content">
+        <div className="auth-bg">
+          <span className="r"></span>
+          <span className="r s"></span>
+          <span className="r s"></span>
+          <span className="r"></span>
         </div>
-    )
-}
+        <div className="card fondo">
+          <div className="card-body text-center fondo">
+            <div className="card-header mx-auto fondo">
+              <img src={logo} className="w-75 h-25 d-inline-block" alt="Logo" />
+            </div>
+            <h3 className="mb-4 color1">Inscribirse</h3>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre de usuario"
+              />
+            </div>
+            <div className="input-group mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+            <div className="input-group mb-4">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="clave"
+              />
+            </div>
 
-export default SignUp
+            <button className="btn btn-primary shadow-2 mb-4">Inscribirse</button>
+            <p className="mb-0 text-muted">
+            ¿Ya tienes una cuenta?<a href="auth-signin.html"> Iniciar sesión</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
